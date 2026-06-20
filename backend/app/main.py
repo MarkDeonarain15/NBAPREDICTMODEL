bc8a80553da3227a14843717eb501dda62e901f9
Mark Deonarain <markshiv0818@gmail.com>
2026-06-20 14:50:04 -0400

    Update backend and lockfile

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI(title="NBA Game Predictor API")
 
+
+@app.get("/")
+def root():
+    return {"message": "NBA Predictor API Running"}
+
+
 @app.get("/health")
 def health():
     return {"ok": True}
